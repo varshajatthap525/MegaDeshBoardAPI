bcfbcafcd661a4aaced7436d3bb13c865d9533f0
varsha <varshajatthap525@gmail.com>
2024-05-03 15:15:32 +0530

    intial commit

---
+++ src/main/java/com/MegaDeshBoardAPI/MegaDeshBoardApiApplication.java
@@ -0,0 +1,13 @@
+package com.MegaDeshBoardAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegaDeshBoardApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegaDeshBoardApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MegaDeshBoardAPI/MegaDeshBoardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MegaDeshBoardAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MegaDeshBoardApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
